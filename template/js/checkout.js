6f08dd068ddef31c443b54d8255325c4f1585035
vitorrgg <vitorrggm@gmail.com>
2024-09-19 10:49:33 -0300

    style: replicating layout

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,10 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+
+const screenWidth = document.body ? document.body.offsetWidth : window.screen.width
+
+if (screenWidth < 992) {
+    navigator.__defineGetter__('userAgent', function () {
+        return "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36"
+    });
+}
